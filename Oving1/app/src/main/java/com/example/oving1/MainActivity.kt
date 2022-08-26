7252f6b2773d96f19dc619d8bf7e99f7bd216969
Mikkel Ofrim <39702459+Mikkelof@users.noreply.github.com>
2022-08-26 23:33:41 +0200

    oving 1 done

--- Oving1/app/src/main/java/com/example/oving1/MainActivity.kt
+++ Oving1/app/src/main/java/com/example/oving1/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.oving1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menu.add("Mikkel")
+        menu.add("Ofrim")
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.title.equals("Mikkel")) {
+            Log.w("Navn","Mikkel")
+        }
+        if (item.title.equals("Ofrim")) {
+            Log.e("Navn", "Ofrim")
+        }
+        return true
     }
 }
