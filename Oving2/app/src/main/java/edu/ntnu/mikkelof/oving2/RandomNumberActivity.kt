bc38d669468fe05da968ad2a73ca5f27f097b8c9
Mikkel Ofrim <39702459+Mikkelof@users.noreply.github.com>
2022-09-01 14:15:55 +0200

    Oving 2_b done

--- Oving2/app/src/main/java/edu/ntnu/mikkelof/oving2/RandomNumberActivity.kt
+++ Oving2/app/src/main/java/edu/ntnu/mikkelof/oving2/RandomNumberActivity.kt
@@ -1,19 +1,20 @@
 package edu.ntnu.mikkelof.oving2
 
 import android.app.Activity
-import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 
-class MainActivity : Activity() {
+class RandomNumberActivity : Activity() {
+    var upperLimit = 100
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(this, randomNum().toString(), Toast.LENGTH_LONG).show()
     }
 
-    fun onClickStartRandomNumberActivity(v: View?) {
-        val intent = Intent(".RandomNumberActivity")
-        startActivity(intent)
+    fun randomNum(): Int {
+        return (0..upperLimit).random()
     }
 }
