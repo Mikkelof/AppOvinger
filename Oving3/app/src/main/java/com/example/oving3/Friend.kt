de6cb2bed68981274b88990235f39e969a542a6e
Mikkel Ofrim <39702459+Mikkelof@users.noreply.github.com>
2022-09-09 21:38:36 +0200

    Adding friends work

--- Oving3/app/src/main/java/com/example/oving3/Friend.kt
+++ Oving3/app/src/main/java/com/example/oving3/Friend.kt
@@ -2,4 +2,8 @@ package com.example.oving3;
 
 import java.io.Serializable
 
-class Friend(var name: String, var dateOfBirth: String): Serializable
+class Friend(var name: String, var dateOfBirth: String): Serializable {
+    override fun toString(): String {
+        return "Name: " + name + ", Date of birth: " + dateOfBirth
+    }
+}
--- Oving3/app/src/main/java/com/example/oving3/MainActivity.kt
+++ Oving3/app/src/main/java/com/example/oving3/MainActivity.kt
@@ -2,12 +2,16 @@ package com.example.oving3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ArrayAdapter
+import android.widget.EditText
 import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
     private var friends: ArrayList<Friend> = ArrayList<Friend>()
     private lateinit var listView: ListView
+    private lateinit var editTextName: EditText
+    private lateinit var editTextDate: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,7 +21,16 @@ class MainActivity : AppCompatActivity() {
         friends.add(Friend("Michael", "17. januar 1988"))
         friends.add(Friend("Miguel", "17. januar 1977"))
 
-        listView = findViewById<ListView>(R.id.listView)
+        listView = findViewById(R.id.listView)
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, friends)
+        listView.adapter = adapter
+    }
+
+    fun onClickAddFriend(v: View?) {
+        editTextName = findViewById(R.id.editTextName)
+        editTextDate = findViewById(R.id.editTextBirthday)
+
+        friends.add(Friend(editTextName.getText().toString(), editTextDate.getText().toString()))
         val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, friends)
         listView.adapter = adapter
     }
