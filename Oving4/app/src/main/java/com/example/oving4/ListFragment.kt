94d328cfe063ff3f1a8c895f5898ac18cde9044f
Mikkel Ofrim <39702459+Mikkelof@users.noreply.github.com>
2022-09-23 18:55:59 +0200

    ListFragment should work now

--- Oving4/app/src/main/java/com/example/oving4/ListFragment.kt
+++ Oving4/app/src/main/java/com/example/oving4/ListFragment.kt
@@ -1,10 +1,14 @@
 package com.example.oving4
 
+import android.content.Context
 import android.os.Bundle
+import android.view.View
 import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.fragment.app.ListFragment
 
 class InfoFragment : ListFragment() {
+    private var mListener: OnFragmentInteractionListener? = null
     private var movies: Array<String> = arrayOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,5 +17,31 @@ class InfoFragment : ListFragment() {
         listAdapter = activity?.let {
             ArrayAdapter(it, android.R.layout.simple_list_item_1, android.R.id.text1, movies)
         }
+    }
+
+    interface OnFragmentInteractionListener {
+
+        fun onFragmentInteraction(index: Int?)
+    }
+
+    override fun onListItemClick(l: ListView, v: View, position: Int, id: Long) {
+        super.onListItemClick(l, v, position, id)
+        mListener!!.onFragmentInteraction(position)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mListener = try {
+            activity as OnFragmentInteractionListener
+        } catch (e: ClassCastException) {
+            throw ClassCastException(
+                "$activity must implement OnFragmentInteractionListener"
+            )
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        mListener = null
     }
 }
